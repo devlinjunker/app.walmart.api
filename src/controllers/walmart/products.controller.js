c0c388adfc449fbc6e782620c7e11dc933ea9438
Devlin Junker <devlin.junker@eduworks.com>
2018-09-17 22:08:00 -0700

    hit walmart endpoint and return list of product ids

--- src/controllers/walmart/products.controller.js
+++ src/controllers/walmart/products.controller.js
@@ -1,11 +1,44 @@
+//@flow
 require('babel-core/register');
 require('babel-polyfill');
 
+import _ from 'lodash';
+
+import rp from 'request-promise';
+
+const apiKey = 'kjybrqfdgp3u4yv2qzcnjndj';
+
+/**
+ * Walmart Product API Controller
+ */
 export class ProductsController {
-  static FindProducts(request: any) {
+  /**
+   * Takes the q parameter from the query string and passes to walmart API as the keyword
+   * @param  {RequestParams}  request request parameters
+   * @return {Promise}        resolves once returning the results from walmart API
+   */
+  static async FindProducts(request: any) {
     const keywordString = request.query.q;
 
-    return keywordString.split(' ');
+    const response = JSON.parse(await rp.get('http://api.walmartlabs.com/v1/search', {
+      q: {
+        apiKey,
+        query: keywordString
+      }
+    }));
+
+    return response.items;
+  }
+
+  /**
+   * Uses the q parameter of query string and returns a list of IDs of products returned from walmart API
+   * @param  {RequestParams}  request request parameters
+   * @return {Promise}         resolves once returning the product ids matching the keyword
+   */
+  static async FindProductIds(request: any) {
+    const response = await ProductsController.FindProducts(request);
+
+    return _.map(response, 'itemId');
   }
 }
 
@@ -14,5 +47,10 @@ export default [
     path: '/walmart/products',
     method: 'GET',
     controller: ProductsController.FindProducts
+  },
+  {
+    path: '/walmart/product/ids',
+    method: 'GET',
+    controller: ProductsController.FindProductIds
   }
 ];
--- src/controllers/walmart/products.controller.spec.js
+++ src/controllers/walmart/products.controller.spec.js
@@ -1,19 +1,48 @@
 //@flow
 import { ProductsController } from './products.controller.js';
 
-import { describe, it } from 'mocha';
+import { describe, it, afterEach } from 'mocha';
 import { expect } from 'chai';
+
+var chai = require('chai');
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+chai.should();
+chai.use(sinonChai);
+
+import rp from 'request-promise';
 
 describe('Products Controller', () => {
   describe('has endpoint FindProducts()', () => {
+
+    const items = [{
+      itemId: '123'
+    }];
+
     it('passes URL query string parameter to walmart API', async () => {
-      const returned = await ProductsController.FindProducts({
+      const getStub = sinon.stub(rp, 'get');
+      getStub.resolves(JSON.stringify({
+        items
+      }));
+
+      await ProductsController.FindProducts({
         query: {
           q: 'keyword'
         }
       });
 
-      expect(returned).to.equal(['keyword']);
+      expect(getStub).to.be.called;
     });
+
+    it('returns items from response', async () => {
+      sinon.stub(rp, 'get').resolves(JSON.stringify({
+        items
+      }));
+    });
+  });
+
+  afterEach(() => {
+    // Restore the default sandbox
+    sinon.restore();
   });
 });
